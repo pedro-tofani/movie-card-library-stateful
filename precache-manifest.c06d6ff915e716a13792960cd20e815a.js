440a5e9d74d3372afbbddbd916fc38874f3f4625
phtf92 <phtf92@gmail.com>
2020-06-01 10:03:49 -0300

    Updates

---
+++ precache-manifest.c06d6ff915e716a13792960cd20e815a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dccac4359c7333b4acc61ba3b273ad1",
+    "url": "/movie-card-library-stateful/index.html"
+  },
+  {
+    "revision": "6c16464f3cc9a6e1beda",
+    "url": "/movie-card-library-stateful/static/css/main.e53567fa.chunk.css"
+  },
+  {
+    "revision": "03277886b908784a5ed4",
+    "url": "/movie-card-library-stateful/static/js/2.a07eac5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-card-library-stateful/static/js/2.a07eac5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c16464f3cc9a6e1beda",
+    "url": "/movie-card-library-stateful/static/js/main.4d3c4769.chunk.js"
+  },
+  {
+    "revision": "729559ddd1efd426b99a",
+    "url": "/movie-card-library-stateful/static/js/runtime-main.86a1c3be.js"
+  }
+]);
